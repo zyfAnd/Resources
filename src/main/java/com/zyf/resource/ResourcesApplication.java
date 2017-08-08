896a62e69d1b0b179fcecbe9538271db6b2c467f
zyfand <1105564280@qq.com>
2017-08-08 11:22:13 +0800

    init data

---
+++ src/main/java/com/zyf/resource/ResourcesApplication.java
@@ -0,0 +1,12 @@
+package com.zyf.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesApplication.class, args);
+	}
+}
